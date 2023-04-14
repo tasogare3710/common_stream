87c12c299d11042fc354d48a9254da8879ede775
tasogare3710 <tasogare.android@gmail.com>
2023-04-14 21:47:42 +0900

    rewrite
    - cargo fix --edition --edition-idioms

--- src/impl.rs
+++ src/impl.rs
@@ -1,28 +1,33 @@
-use crate::Stream;
 use std::{collections::VecDeque, iter::Iterator, marker::PhantomData};
 
-pub struct CommonStream<I, T, U, E, F>
+/// 入力イテレータ`Iterator<Item = T>`と変換関数`FnMut(T) -> Result<U, E>`を使用して`U`の値を返す出力イテレータの実装です。
+/// 型変数`T`と`E`が`?Sized`ではない事に注意してください。このイテレータが`Box<dyn std::error::Error>`を返す事は出来ません。
+pub struct Transformed<I, T, U, E, F>
 where
     I: Iterator<Item = T>,
     F: FnMut(T) -> Result<U, E>,
 {
     iter: I,
-    decode: F,
+    transform: F,
     backing_store: VecDeque<U>,
     phantom: PhantomData<Result<U, E>>,
 }
 
-impl<I, T, U, E, F> crate::UnRead<U> for CommonStream<I, T, U, E, F>
+impl<I, T, U, E, F> crate::UnRead<U> for Transformed<I, T, U, E, F>
 where
     I: Iterator<Item = T>,
     F: FnMut(T) -> Result<U, E>,
 {
     fn unread(&mut self, token: U) {
-        self.backing_store.push_front(token);
+        self.backing_store.push_back(token);
+    }
+
+    fn unread_from_tokens(&mut self, iter: impl IntoIterator<Item = U>) {
+        self.backing_store.extend(iter)
     }
 }
 
-impl<I, T, U, E, F> Iterator for CommonStream<I, T, U, E, F>
+impl<I, T, U, E, F> Iterator for Transformed<I, T, U, E, F>
 where
     I: Iterator<Item = T>,
     F: FnMut(T) -> Result<U, E>,
@@ -32,91 +37,128 @@ where
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.backing_store.is_empty() {
-            self.iter.next().map(|it| (self.decode)(it))
+            self.iter.next().map(|it| (self.transform)(it))
         } else {
-            self.backing_store.pop_front().map(Ok)
+            self.backing_store.pop_back().map(Ok)
         }
     }
 }
 
-impl<I, T, U, E, F> Stream<U, E> for CommonStream<I, T, U, E, F>
+use std::fmt;
+
+impl<I, T, U, E, F> fmt::Debug for Transformed<I, T, U, E, F>
 where
     I: Iterator<Item = T>,
     F: FnMut(T) -> Result<U, E>,
-    E: std::error::Error + Send + Sync + 'static,
+    T: fmt::Debug,
+    U: fmt::Debug,
 {
-}
-
-#[derive(Debug)]
-pub struct Builder<I, T, U, E, F>
-where
-    I: Iterator<Item = T>,
-    F: FnMut(T) -> Result<U, E>,
-{
-    iter: Option<I>,
-    decode: Option<F>,
-    backing_store: Option<VecDeque<U>>,
-}
-
-impl<I, T, U, E, F> Builder<I, T, U, E, F>
-where
-    I: Iterator<Item = T>,
-    F: FnMut(T) -> Result<U, E>,
-{
-    pub fn build(self) -> CommonStream<I, T, U, E, F> {
-        if let Builder {
-            iter: Some(iter),
-            decode: Some(decode),
-            backing_store,
-        } = self
-        {
-            CommonStream {
-                iter,
-                decode,
-                backing_store: backing_store.unwrap_or_else(VecDeque::new),
-                phantom: Default::default(),
-            }
-        } else {
-            // XXX: エラーを返す必要がある
-            panic!();
-        }
-    }
-
-    pub fn iter(mut self, iter: I) -> Self {
-        self.iter.replace(iter);
-        self
-    }
-
-    pub fn decode(mut self, decode: F) -> Self {
-        self.decode.replace(decode);
-        self
-    }
-
-    pub fn backing_store(mut self, backing_store: VecDeque<U>) -> Self {
-        self.backing_store.replace(backing_store);
-        self
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("CommonStream")
+            .field("backing_store", &self.backing_store)
+            .field("phantom", &self.phantom)
+            .finish()
     }
 }
 
-impl<I, T, U, E, F> CommonStream<I, T, U, E, F>
+impl<I, T, U, E, F> Transformed<I, T, U, E, F>
 where
     I: Iterator<Item = T>,
     F: FnMut(T) -> Result<U, E>,
 {
-    pub fn new(iter: I, decode: F) -> Self {
+    pub fn new(iter: I, transform: F) -> Self {
         Self {
             iter,
-            decode,
+            transform,
             backing_store: Default::default(),
-            phantom: Default::default(),
+            phantom: PhantomData,
         }
     }
 
-    pub fn build() -> Builder<I, T, U, E, F> {
-        Builder {
-            iter: None,
-            decode: None,
-            backing_store: None,
+    pub fn with_backing_store(iter: I, transform: F, backing_store: impl IntoIterator<Item = U>) -> Self {
+        Self {
+            iter,
+            transform,
+            backing_store: VecDeque::from_iter(backing_store),
+            phantom: PhantomData,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ::{
+        std::{
+            collections::VecDeque,
+            io::{self, BufRead as _, BufReader},
+            iter::Iterator,
+            string::FromUtf8Error,
+        },
+        thiserror::Error,
+    };
+
+    #[cfg(test)]
+    struct BufReadIter {
+        reader: BufReader<VecDeque<u8>>,
+    }
+
+    #[cfg(test)]
+    impl FromIterator<u8> for BufReadIter {
+        fn from_iter<T: IntoIterator<Item = u8>>(iter: T) -> Self {
+            Self {
+                reader: BufReader::new(VecDeque::from_iter(iter)),
+            }
+        }
+    }
+
+    #[cfg(test)]
+    impl Iterator for BufReadIter {
+        type Item = Result<Vec<u8>, io::Error>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            let mut buf = Vec::with_capacity(80);
+            match self.reader.read_until(0xA, &mut buf) {
+                Ok(len) => {
+                    if len == 0 {
+                        None
+                    } else {
+                        let last = buf.pop();
+                        assert!(last.is_some());
+                        assert!(!buf.is_empty());
+                        Some(Ok(buf))
+                    }
+                }
+                Err(err) => Some(Err(err.into())),
+            }
+        }
+    }
+
+    #[cfg(test)]
+    #[derive(Error, Debug)]
+    pub enum TransformError {
+        #[error("{0:?}")]
+        IoError(#[from] io::Error),
+        #[error("{0:?}")]
+        FromUtf8Error(#[from] FromUtf8Error),
+    }
+
+    #[cfg(test)]
+    fn tranform(bytes: Result<Vec<u8>, io::Error>) -> Result<String, TransformError> {
+        match bytes {
+            Ok(bytes) => String::from_utf8(bytes).map_err(TransformError::from),
+            Err(err) => Err(TransformError::from(err)),
+        }
+    }
+
+    #[test]
+    fn utf8_encoded_stream_to_multi_string_stream() {
+        let stream = "apple\u{0A}grape\u{0A}banana\u{0A}";
+        let stream = BufReadIter::from_iter(stream.bytes());
+        let mut stream = super::Transformed::new(stream, tranform);
+
+        assert_eq!("apple", stream.next().unwrap().unwrap().as_str());
+        assert_eq!("grape", stream.next().unwrap().unwrap().as_str());
+        assert_eq!("banana", stream.next().unwrap().unwrap().as_str());
+        assert!(stream.next().is_none());
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,88 @@
+//! `Iterator<Item = Result<T, E>>`型に追加の制約を与えることで任意のストリームから任意のトークンの読み出しを実現するクレート。
+//!
+//! [抽象化](self::Stream)と[エラーに関する更なる追加の制約を与えるトレイト](self::SendSyncStream)と[単純で汎用のイテレータ実装](self::Transformed)で構成される。
 mod r#impl;
 
-pub use r#impl::{Builder, CommonStream};
+pub use r#impl::Transformed;
 
 use std::iter::{IntoIterator, Iterator};
 
+/// 恒等関数
+///
+/// ```
+/// use common_stream::{ident, Transformed};
+///
+/// let s = [0x006d, 0x0075, 0x0073, 0x0069, 0x0063].iter().cloned();
+/// let s = Transformed::new(char::decode_utf16(s), ident);
+///
+/// assert_eq!(String::from("music"), s.map(Result::unwrap).collect::<String>());
+/// ```
+pub fn ident<X>(x: X) -> X {
+    x
+}
+
 /// 抽象化のためのトレイト
-pub trait Stream<T, E>
+pub trait Stream<TK, E>
 where
-    E: std::error::Error, // + Send + Sync + 'static
-    Self: Iterator<Item = Result<T, E>>,
+    Self: Iterator<Item = Result<TK, E>>,
 {
 }
 
-/// プッシュバックを可能とするトレイト。
-pub trait UnRead<T> {
+impl<T, TK, E> Stream<TK, E> for T
+where
+    E: std::error::Error,
+    T: Iterator<Item = Result<TK, E>>,
+{
+}
+
+/// `E`が[Send]と[Sync]を実装した[Stream](self::Stream)。
+pub trait SendSyncStream<TK, E>
+where
+    Self: Iterator<Item = Result<TK, E>>,
+{
+}
+
+impl<T, TK, E> SendSyncStream<TK, E> for T
+where
+    E: std::error::Error + Send + Sync + 'static,
+    T: Iterator<Item = Result<TK, E>>,
+{
+}
+
+/// プッシュバック機能を追加するトレイトです。
+///
+/// このトレイトのメソッドから戻ると次に読み込まれる値はプッシュバックされたトークンと同じになります。
+pub trait UnRead<U> {
     /// 一つのトークンをプッシュバックします。
-    /// このメソッドから返ると次に読み込まれる値はこのメソッドの引数`token`と同じになります。
     ///
     /// # Parameters
-    /// ## `token`
-    /// プッシュバックするトークンです。
-    fn unread(&mut self, token: T);
+    /// <dl>
+    /// <dt>token</dt>
+    /// <dd>プッシュバックするトークンです。</dd>
+    /// </dl>
+    fn unread(&mut self, token: U);
 
     /// 複数のトークンをプッシュバックします。
-    /// このメソッドから返るとそれ以降に読み込まれる値は`iter`が返したトークンと同じになります。
     ///
     /// # Parameters
-    /// ## `iter`
-    /// プッシュバックするトークンです。
-    ///
-    /// このイテレーターが返した個数だけ順序通りに値を返すようになります。
-    fn unread_from_tokens(&mut self, iter: impl IntoIterator<Item = T>) {
-        for c in iter {
-            self.unread(c)
-        }
-    }
+    /// <dl>
+    /// <dt>iter</dt>
+    /// <dd>このイテレーターが返した個数だけ順序通りにトークンを返すようになります。</dd>
+    /// </dl>
+    fn unread_from_tokens(&mut self, iter: impl IntoIterator<Item = U>);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::CommonStream;
-    use std::char;
+    use super::{ident, Transformed};
+    use std::char::decode_utf16;
 
     #[test]
     fn unread_all() {
         use super::UnRead as _;
 
         let s = [0x006d, 0x0075, 0x0073, 0x0069, 0x0063].iter().cloned();
-        let mut s = CommonStream::new(char::decode_utf16(s), |x| x);
+        let mut s = Transformed::new(decode_utf16(s), ident);
         let mut list = std::collections::VecDeque::<char>::new();
 
         let ch = s.next().map(Result::ok).unwrap().unwrap();
@@ -82,15 +117,24 @@ mod tests {
     #[test]
     fn stream_from_iter_decode_identify() {
         let s = [0x006d, 0x0075, 0x0073, 0x0069, 0x0063].iter().cloned();
-        let s = CommonStream::new(char::decode_utf16(s), |x| x);
+        let s = Transformed::new(decode_utf16(s), ident);
 
         assert_eq!(String::from("music"), s.map(Result::unwrap).collect::<String>());
     }
 
     #[test]
     fn stream_from_iter_infallible() {
-        let s = CommonStream::<_, char, _, std::convert::Infallible, _>::new("music".chars(), Result::Ok);
+        let s = Transformed::<_, char, _, std::convert::Infallible, _>::new("music".chars(), Result::Ok);
 
         assert_eq!(String::from("music"), s.map(Result::unwrap).collect::<String>());
+    }
+
+    #[cfg(test)]
+    fn consume_send_sync_stream<TK, E>(_stream: impl super::SendSyncStream<TK, E>) {}
+
+    #[test]
+    fn pass_send_sync_stream_trait() {
+        let s = Transformed::new(std::iter::empty::<Result<u8, std::convert::Infallible>>(), ident);
+        consume_send_sync_stream(s);
     }
 }
